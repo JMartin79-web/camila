429931780c48231b197105500d74ab34814d3f1c
martin <jmartinaguirre79@gmail.com>
2022-12-24 14:25:38 -0300

    Para revision

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,7 +1,14 @@
-const hamburger = document.querySelector(".hamburger")
+const header = document.getElementById("header")
 const header2 = document.getElementById("header2")
+
+const hamburger = document.querySelector(".hamburger")
 
 function handleToggle(){
     hamburger.classList.toggle("is-active")
     header2.classList.toggle("header2-active")
+    if(header2.classList.contains("header2-active")){
+        header.classList.add("header-open")
+    }else{
+        header.classList.remove("header-open")
+    }
 }
