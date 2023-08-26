03f4d917cfc305907889a1ac52388eff63e0eb7d
gabrielTor <gabriel_torres2012@yahoo.com>
2023-08-26 15:38:08 -0300

    Nextjs

---
+++ components/Carrousel.jsx
@@ -0,0 +1,270 @@
+import Image from 'next/image'
+import img1 from "../assets/fotos/tour1-grande.jpg"
+import img2 from "../assets/fotos/tour_montaña_2.jpg"
+import img3 from "../assets/fotos/la_alejandra_2.jpg"
+import magic from "../assets/fotos/magia.png"
+
+// const grande = document.querySelector('.grande')
+// const punto = document.querySelectorAll('.punto')
+
+// punto.forEach( ( cadaPunto , i )=> {
+//     // Asignamos un CLICK a cadaPunto
+//     punto[i].addEventListener('click',()=>{
+
+//         // Guardar la posición de ese PUNTO
+//         let posicion  = i
+//         // Calculando el espacio que debe DESPLAZARSE el GRANDE
+//         let operacion = posicion * -33.3
+
+//         // MOVEMOS el grand
+//         grande.style.transform = `translateX(${ operacion }%)`
+//         console.log("operacion: ", operacion)
+//         // Recorremos TODOS los punto
+//         punto.forEach( ( cadaPunto , i )=>{
+//             // Quitamos la clase ACTIVO a TODOS los punto
+//             punto[i].classList.remove('activo')
+//         })
+//         // Añadir la clase activo en el punto que hemos hecho CLICK
+//         punto[i].classList.add('activo')
+
+//     })
+// })
+
+// //  LOGICA PARA MOSTRAR LOS TEMPLATES
+// const punto1 = document.getElementById("punto1")
+// const punto2 = document.getElementById("punto2")
+// const punto3 = document.getElementById("punto3")
+
+// // div donde va a ir el fragment
+// const div = document.getElementById("tour-info")
+// let fragment = document.createDocumentFragment();
+
+// // templates
+// const template1 = document.getElementById("template-1").content
+// const template2 = document.getElementById("template-2").content
+// const template3 = document.getElementById("template-3").content
+
+// // al principio tiene abierto el template 1
+// const defaultTemplate = template1.cloneNode(true);
+// fragment.appendChild(defaultTemplate);
+// div.appendChild(fragment);
+
+// // manejar el ver mas
+// function handleTourBtn (){
+//     const tourBtn = document.querySelector(".tour-btn")
+//     const tourDiv = document.querySelector(".tour-div")
+//     tourBtn.innerHTML = "See more"
+//     if(tourDiv.classList.contains("tour-div-hiden")){
+//         console.log("contiene tour-div-hiden")
+//         tourBtn.innerHTML = "X"
+//         tourDiv.classList.remove("tour-div-hiden")
+//     }else{
+//         console.log("no contiene tour-div-hiden")
+//         tourBtn.innerHTML = "See more"
+//         tourDiv.classList.add("tour-div-hiden")
+//     }
+// }
+
+// // POR DEFECTO
+// div.innerHTML = "";
+// console.log("Punto 1 contiene la clase activo")
+// const clone = template1.cloneNode(true);
+// fragment.appendChild(clone);
+// div.appendChild(fragment);
+
+// punto.forEach( (cadaPunto) =>{
+//     cadaPunto.addEventListener("click", ()=>{
+
+//         if(punto1.classList.contains("activo")){
+//             div.innerHTML = "";
+//             console.log("Punto 1 contiene la clase activo")
+//             const clone = template1.cloneNode(true);
+//             fragment.appendChild(clone);
+//             div.appendChild(fragment);
+//         }
+
+//         if(punto2.classList.contains("activo")){
+//             div.innerHTML = "";
+//             console.log("Punto 2 contiene la clase activo")
+//             const clone = template2.cloneNode(true);
+//             fragment.appendChild(clone);
+//             div.appendChild(fragment);
+//         }
+
+//         if(punto3.classList.contains("activo")){
+//             div.innerHTML = "";
+//             console.log("Punto 3 contiene la clase activo")
+//             const clone = template3.cloneNode(true);
+//             fragment.appendChild(clone);
+//             div.appendChild(fragment);
+//         }
+//     })
+// })
+
+// const arrowBack = document.getElementById("flecha-atras")
+// const arrowFront = document.getElementById("flecha-adelante")
+
+// arrowBack.addEventListener("click", arrow =>{
+
+//     // punto 3 a 2
+//     if(punto3.classList.contains("activo")){
+//         arrow.stopPropagation()
+//         let operacion = -33.3
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto3.classList.remove('activo')
+//         punto2.classList.add('activo')
+
+//         console.log("Punto 2 contiene la clase activo")
+//         const clone = template2.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+//     // punto 2 a 1
+//     else if(punto2.classList.contains("activo")){
+//         arrow.stopPropagation()
+//         let operacion = -0
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto2.classList.remove('activo')
+//         punto1.classList.add('activo')
+
+//         console.log("Punto 1 contiene la clase activo")
+//         const clone = template1.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+
+//     // punto 1 a 3
+//     else{
+//         arrow.stopPropagation()
+//         let operacion = -66.6
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto1.classList.remove('activo')
+//         punto3.classList.add('activo')
+
+//         console.log("Punto 3 contiene la clase activo")
+//         const clone = template3.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+// })   
+
+
+// arrowFront.addEventListener("click", arrow =>{
+
+//     // punto 1 a 2
+//     if(punto1.classList.contains("activo")){
+//         arrow.stopPropagation()
+//         let operacion = -33.3
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto1.classList.remove('activo')
+//         punto2.classList.add('activo')
+
+//         console.log("Punto 2 contiene la clase activo")
+//         const clone = template2.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+//     // punto 2 a 3
+//     else if(punto2.classList.contains("activo")){
+//         arrow.stopPropagation()
+//         let operacion = -66.6
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto2.classList.remove('activo')
+//         punto3.classList.add('activo')
+
+//         console.log("Punto 3 contiene la clase activo")
+//         const clone = template3.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+
+//     // punto 3 a 1
+//     else{
+//         arrow.stopPropagation()
+//         let operacion = -0
+//         grande.style.transform =`translateX(${ operacion }%)`
+//         div.innerHTML = "";
+
+//         punto3.classList.remove('activo')
+//         punto1.classList.add('activo')
+
+//         console.log("Punto 1 contiene la clase activo")
+//         const clone = template1.cloneNode(true);
+//         fragment.appendChild(clone);
+//         div.appendChild(fragment);
+//     }
+
+// })   
+
+export default function Carrousel() {
+    return (
+        <div className="contains-carrousel">
+
+            <div className="carrousel-title">
+                <div className="bg-div-title">
+                    <div className="bg-title"></div>
+                    <div className="bg-title"></div>
+                </div>
+
+                <div className="title-txt">
+
+                    <h2 id="tours">Our tours
+                        <div className="contains-magia">
+                            <div className="magia">
+                                <Image width={25} src={magic} alt="" />
+                            </div>
+
+                        </div>
+                    </h2>
+                </div>
+            </div>
+
+            <div className="carrousel-div">
+                <div className="carrousel">
+                    <div className="grande">
+
+
+                        <div className="carrousel-div-img">
+                            <Image width={'100%'} height={'100%'} src={img1} alt="Imagen 1" className="img" />
+                        </div>
+
+                        <div className="carrousel-div-img">
+                            <Image width={'100%'} height={'100%'} src={img2} alt="Imagen 2" className="img" />
+                        </div>
+
+                        <div className="carrousel-div-img">
+                            <Image width={'100%'} height={'100%'} src={img3} alt="Imagen 3" className="img" />
+                        </div>
+
+                    </div>
+
+                    <div className="contains-puntos">
+                        <ul className="puntos">
+                            <li id="flecha-atras" className="flecha"></li>
+                            <li id="punto1" className="punto activo"></li>
+                            <li id="punto2" className="punto"></li>
+                            <li id="punto3" className="punto"></li>
+                            <li id="flecha-adelante" className="flecha"></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id="tour-info">
+
+            </div>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,211 @@
+import Head from "next/head"
+import mendoza from "../assets/fotos/hero_img2.jpg"
+import magic from "../assets/fotos/magia.png"
+import magicTitle from "../assets/fotos/maiga-title.png"
+import Image from "next/image"
+import Tour from "@/components/Tour"
+import Carrousel from "@/components/Carrousel"
+import { useEffect } from "react"
+
+export default function Home() {
+
+  useEffect(() => {
+    // Google Tag Manager script
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NBLDKCL');
+    `;
+    document.head.appendChild(script);
+
+    // Google Tag Manager noscript code
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NBLDKCL"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      />
+    `;
+    document.body.appendChild(noscript);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Your friend in Mendoza</title>
+      </Head>
+      <main>
+        {/* <!-- HEADER --> */}
+        <div className="contains-header" id="header">
+
+          <div className="header1">
+            <header className="header">
+              <div className="header-logo">
+                {/* <!-- <a href="#">CAMILA</a> --> */}
+              </div>
+              <div className="header-links">
+                <a href="#about">About</a>
+                <a href="#tours">Tours</a>
+                <a href="#contact">Contact</a>
+              </div>
+              <div className="header-menu">
+                <button className="hamburger hamburger--squeeze" type="button">
+                  <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                  </span>
+                </button>
+              </div>
+            </header>
+          </div>
+
+          <div id="header2" className="header2">
+            <div className="header2-links">
+              <a href="#about">About</a>
+              <a href="#tours">Tours</a>
+              <a href="#contact">Contact</a>
+              <br />
+              {/* <!-- <p>261 696-4118</p> --> */}
+              {/* <!-- <p>Barrio Furlotti Manzana O casa 14, Maipú, Mendoza</p> --> */}
+            </div>
+          </div>
+
+        </div>
+
+        {/* <!-- HERO SECTION --> */}
+        <div className="contains-hero">
+          <div className="hero-img">
+            <div className="hero-img-filter"></div>
+            <Image src={mendoza} alt="mendoza" />
+          </div>
+          <section className="hero">
+            <h1>Your friend in<br />Mendoza</h1>
+            <p className="hero-p">Let's explore together!</p>
+            <button className="button">
+              <a href='https://api.whatsapp.com/send/?phone=5492616964118&text&app_absent=0' target="_blank">LETS TALK</a>
+            </button>
+          </section>
+        </div>
+
+        {/* <!-- CARROUSEL SECTION --> */}
+        <Carrousel />
+
+        {/* <!-- ABOUT SECTIO --> */}
+        <div className="contains-about">
+          <section className="about">
+
+            {/* <!--  
+            <div className="about-contains-img">
+                <div className="about-rectangle" id="about">
+                </div>
+                <div className="about-img">
+                    <img src="./assets/fotos/about/camila.png" alt="perfil camila">
+                </div>
+
+                <div className="about-img2">
+                    <img src="./assets/fotos/about/rodrigo.png" alt="perfil rodrigo">
+                </div>
+            </div>
+            --> */}
+
+            <div className="about-contains-info">
+              <h2 className="about-tittle-mobile">Hello! We are your Mendocinians friends.</h2>
+              <h2 className="about-h">Hello! We are your</h2>
+              <h2 className="about-h-big">Mendocinians friends.</h2>
+              <p className="about-p">
+                We are a group made up of 3 partners and friends who are passionate about our province and for making all those who visit it take away beautiful memories.
+                <br /><br />
+
+                We can and we want to be more than your guides or drivers. We want to take care of you and adapt to your preferences and needs so your days in Mendoza are the happiest and most memorable.
+                <br /><br />
+
+                Team up with us and book a tour with recommendations that only locals know.
+                <br />
+                Even if you want us to put together a personalized tour for you, feel free to ask us and we will gladly help you.
+              </p>
+            </div>
+          </section>
+        </div>
+
+        {/* <!-- PLUS --> */}
+        <div className="contains-plus">
+          <div className="plus">
+            <div className="plus-title">
+              <h2><div className="contains-plus-magia">
+                <div className="plus-magia">
+                  <Image width={25} src={magic} alt="" />
+                </div>
+              </div>Plus
+                <br />
+                services</h2>
+            </div>
+            <div className="plus-txt">
+              <p>Book one of my tours or have a free video call with me! You'll get free travel advice, more recommendations and even personalised plans. </p>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- CALL TO ACTION --> */}
+        <div className="contains-call">
+          <section className="call">
+            <Image className="call-img" src={magicTitle} alt="magic title" />
+            <h2 id="contact">Let´s make it happen!</h2>
+            <p>We'll make sure it's your best trip ever!</p>
+            {/* <!-- <button className="button"><a href="mailto:yourfriendinmendoza@gmail.com">EMAIL ME</a></button> --> */}
+            <button className="button"><a href='https://api.whatsapp.com/send/?phone=5492616964118&text&app_absent=0' target="_blank">CONTACT US</a></button>
+          </section>
+        </div>
+
+        {/* <!-- FOOTER --> */}
+        <div className="contains-footer">
+          <footer className="footer">
+
+            <div className="footer-div1">
+              <br />
+              <h2>Your friend in<br />Mendoza</h2>
+              <br />
+              <p className="footer-p">Thank you for visiting</p>
+              {/* <!-- <p className="footer-p">261 696-4118 | Barrio Furlotti Manzana O casa 14, Maipú, Mendoza</p> --> */}
+            </div>
+
+            {/* <!--
+                CODIGO ANTERIOR
+                
+                <div className="footer-separation"></div>
+                
+                <div className="footer-div2">
+                    <h2>Your friend in<br/>Mendoza</h2>
+                    <br/>
+                </div>
+                
+             --> */}
+
+            <div className="footer-separation"></div>
+
+            <div className="footer-div3">
+              <a href="http://qr.afip.gob.ar/?qr=Q49RqIpafI1e_gqBJT4NrA,," target="_F960AFIPInfo">
+                <img src="http://www.afip.gob.ar/images/f960/DATAWEB.jpg" />
+              </a>
+            </div>
+          </footer>
+
+        </div>
+
+        {/* <!-- BOTON WHATSAPP --> */}
+        <a href="https://api.whatsapp.com/send/?phone=5492616964118&text&app_absent=0" target="_blank" id="btn-wsp">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M11.42 9.49c-.19-.09-1.1-.54-1.27-.61s-.29-.09-.42.1-.48.6-.59.73-.21.14-.4 0a5.13 5.13 0 0 1-1.49-.92 5.25 5.25 0 0 1-1-1.29c-.11-.18 0-.28.08-.38s.18-.21.28-.32a1.39 1.39 0 0 0 .18-.31.38.38 0 0 0 0-.33c0-.09-.42-1-.58-1.37s-.3-.32-.41-.32h-.4a.72.72 0 0 0-.5.23 2.1 2.1 0 0 0-.65 1.55A3.59 3.59 0 0 0 5 8.2 8.32 8.32 0 0 0 8.19 11c.44.19.78.3 1.05.39a2.53 2.53 0 0 0 1.17.07 1.93 1.93 0 0 0 1.26-.88 1.67 1.67 0 0 0 .11-.88c-.05-.07-.17-.12-.36-.21z" />
+            <path d="M13.29 2.68A7.36 7.36 0 0 0 8 .5a7.44 7.44 0 0 0-6.41 11.15l-1 3.85 3.94-1a7.4 7.4 0 0 0 3.55.9H8a7.44 7.44 0 0 0 5.29-12.72zM8 14.12a6.12 6.12 0 0 1-3.15-.87l-.22-.13-2.34.61.62-2.28-.14-.23a6.18 6.18 0 0 1 9.6-7.65 6.12 6.12 0 0 1 1.81 4.37A6.19 6.19 0 0 1 8 14.12z" />
+          </svg>
+        </a>
+
+        {/* <Tour /> */}
+      </main>
+    </>
+
+  )
+}
